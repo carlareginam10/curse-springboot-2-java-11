6c3fabd411f84898dc96f30a1832f03c5d5e80cb
carla regina <carlareginam10@gmail.com>
2021-02-16 16:30:42 -0300

    Service Layer, component registration

--- src/main/java/com/educandoweb/curse/resources/UserResourse.java
+++ src/main/java/com/educandoweb/curse/resources/UserResourse.java
@@ -1,11 +1,16 @@
 package com.educandoweb.curse.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.curse.entities.User;
+import com.educandoweb.curse.services.UserService;
 
 //Essa classe disponibiliza um recurso web correspondente a entidade User
 
@@ -17,14 +22,25 @@ import com.educandoweb.curse.entities.User;
 @RequestMapping (value ="/users")
 public class UserResourse {
 	
+	//injetando dependencia para o service por meio da anotation - UserResourse depende do UserService
+	@Autowired
+	private UserService service;
+	
 	//método endpoint pra acessar os usuários
 	//ResponseEntity tipo específico do spring pra retornar respostas de requisições web
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		// o L na frente do 1 quer dizer que é um long
-		User u = new User(1L, "Maria","maria@gmail", "99999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List> findAll(){
+		
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
 	}
 	
+	//indica que vou passar um parametro na url
+	@GetMapping(value = "/{id}")
+	public ResponseEntity <User> findById(@PathVariable Long id){
+		User obj = service.finBydId(id);
+		return ResponseEntity.ok().body(obj);
+	}
 
 }
---
+++ src/main/java/com/educandoweb/curse/services/UserService.java
@@ -0,0 +1,31 @@
+package com.educandoweb.curse.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.curse.entities.User;
+import com.educandoweb.curse.repositories.UserRepository;
+
+// A classe tem que estar registrada como um componente do spring (através da anotation @Component, @Repository ou @Service)
+//para que a injeção de dependencialá no UserResouces funcione - escolha a anotation de acordo com o tipo da sua classe
+@Service
+public class UserService {
+	
+	//injeção de dependência - UserService depende de UserRepository
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User finBydId(Long id){
+		Optional <User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
