283a0184a1e03925c00a7cc8056e16a285590556
carla regina <carlareginam10@gmail.com>
2021-02-17 13:27:48 -0300

    User update

--- src/main/java/com/educandoweb/curse/resources/UserResourse.java
+++ src/main/java/com/educandoweb/curse/resources/UserResourse.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,5 +62,12 @@ public class UserResourse {
 		//o metodo noContente retorna uma resposta vazia codigo http 204
 		return ResponseEntity.noContent().build();
 	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> upadate(@PathVariable Long id, @RequestBody User obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 
 }
