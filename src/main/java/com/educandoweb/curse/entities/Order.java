15c458938575992650d5964beb79d7e7443e866d
carla regina <carlareginam10@gmail.com>
2021-02-16 22:35:45 -0300

    OrdemItem, many-to-many association with extra attibutes

--- src/main/java/com/educandoweb/curse/entities/Order.java
+++ src/main/java/com/educandoweb/curse/entities/Order.java
@@ -2,6 +2,8 @@ package com.educandoweb.curse.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.educandoweb.curse.entities.enums.OrderStatus;
@@ -37,6 +40,13 @@ public class Order implements Serializable {
 	//informar qual o nome da chave estrangeira no banco
 	@JoinColumn(name = "client_Id")
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
+	public Set<OrderItem> getItems(){
+		return items;
+	}
 	
 	
 	public Order() {
---
+++ src/main/java/com/educandoweb/curse/repositories/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package com.educandoweb.curse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.curse.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+	
+	
+
+}
