6e831585818a568fa62bc1aaae64811db566c414
carla regina <carlareginam10@gmail.com>
2021-02-16 21:18:41 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/curse/entities/Category.java
+++ src/main/java/com/educandoweb/curse/entities/Category.java
@@ -8,8 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table (name = "tb_category")
@@ -23,8 +25,8 @@ public class Category implements Serializable{
 	private String name;
 	
 	//dentro da categoria eu tenho um conjunto de produtos
-	// a anotação @Transient evita que o java tente intepretar e grave no banco sem dar erro
-	@Transient
+	@JsonIgnore 
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
